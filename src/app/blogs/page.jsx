f14361b93e40c0e7f6379b61ea7cfcb4f15bdcb3
JoaoEnrique <joaoenriquealves13@gmail.com>
2025-12-31 17:14:36 -0300

    fix: actions

--- src/app/blogs/page.jsx
+++ src/app/blogs/page.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { databases, COLLECTION_ID, DATABASE_ID, Query } from '@/lib/appwrite';
+// import { databases, COLLECTION_ID, DATABASE_ID, Query } from '@/lib/appwrite';
 import BlogsFilter from '@/components/custom/BlogsFilter';
 
 export const metadata = {
@@ -27,32 +27,33 @@ export const metadata = {
 
 const DEFAULT_IMAGE = '/images/blog/placeholder.png'; // Placeholder image
 
-async function getBlogPosts() {
-  try {
-    const response = await databases.listDocuments(
-      DATABASE_ID,
-      COLLECTION_ID,
-      [Query.orderDesc('createdAt')]
-    );
-    console.log('getBlogPosts', response.documents);
-    return response.documents;
-  } catch (error) {
-    console.error('Error fetching blog posts:', error);
-    return [];
-  }
-}
+// async function getBlogPosts() {
+//   try {
+//     const response = await databases.listDocuments(
+//       DATABASE_ID,
+//       COLLECTION_ID,
+//       [Query.orderDesc('createdAt')]
+//     );
+//     console.log('getBlogPosts', response.documents);
+//     return response.documents;
+//   } catch (error) {
+//     console.error('Error fetching blog posts:', error);
+//     return [];
+//   }
+// }
 
 export default async function BlogsPage() {
-  const blogs = await getBlogPosts();
+  // const blogs = await getBlogPosts();
 
   return (
-    <div className="container mx-auto px-4 py-8 lg:px-16 md:px-16 sm:px-10">
-      <div className="max-w-7xl mx-auto">
-        <h1 className="text-4xl font-bold mb-8 bg-clip-text text-transparent bg-gradient-to-r from-gradientstart/60 to-50% to-gradientend/60">
-          Blog
-        </h1>
-        <BlogsFilter blogs={blogs} defaultImage={DEFAULT_IMAGE} />
-      </div>
-    </div>
+    <></>
+    // <div className="container mx-auto px-4 py-8 lg:px-16 md:px-16 sm:px-10">
+    //   <div className="max-w-7xl mx-auto">
+    //     <h1 className="text-4xl font-bold mb-8 bg-clip-text text-transparent bg-gradient-to-r from-gradientstart/60 to-50% to-gradientend/60">
+    //       Blog
+    //     </h1>
+    //     <BlogsFilter blogs={blogs} defaultImage={DEFAULT_IMAGE} />
+    //   </div>
+    // </div>
   );
 } 
